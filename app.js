bc82a70284542799e6fc438a51f39aa48a85fdd5
Owen Garland <garland.owen@gmail.com>
2020-11-03 17:49:01 +0100

    fix: run on one port for ease of use

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const { createServer } = require('http')
+const app = require('http').createServer((_req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+    createReadStream('index.html').pipe(res)
+  })
+const io = require('socket.io')(app);
 const { createReadStream } = require('fs')
 
-createServer(function (_req, res) {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  createReadStream('index.html').pipe(res)
-}).listen(8080);
 
-const io = require('socket.io')
-const server = io.listen(8081)
+app.listen(8888);
 
 let count = 0
 
-server.on('connection', (ws) => {
+io.on('connection', (ws) => {
   ws.active = false
 
-  ws.on('touch', (message) => {
+  ws.on('touch', () => {
     ws.active = !ws.active
     count += ws.active ? 1 : -1
-    server.sockets.emit('count', count)
+    io.sockets.emit('count', count)
   })
 
   ws.on('disconnect', () => {
     if (ws.active) {
       count--
-      server.sockets.emit('count', count)
+      io.sockets.emit('count', count)
     }
   })
 
   ws.emit('count', count)
 })
+
